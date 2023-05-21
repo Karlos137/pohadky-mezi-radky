dc051751fceb36b191b10f1dbdb91871a7a78ec4
Karel Kvítek <karel.kvitek@seznam.cz>
2023-05-21 08:21:58 +0200

    Fairy tale image edit

--- components/Image/Image.tsx
+++ components/Image/Image.tsx
@@ -3,7 +3,15 @@ import NextImage, { ImageProps as NextImageProps } from "next/image"
 interface ImageProps extends NextImageProps {}
 
 const Image = (props: ImageProps) => {
-  return <NextImage {...props} />
+  return (
+    <div className="relative aspect-video w-full rounded-2xl">
+      <NextImage
+        {...props}
+        fill={true}
+        className="!m-0 rounded-2xl object-cover object-center"
+      />
+    </div>
+  )
 }
 
 export default Image
