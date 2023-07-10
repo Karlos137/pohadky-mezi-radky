d201cd5e191e130982bdafbce7cfc414b46ec743
Karel Kvítek <karel.kvitek@seznam.cz>
2023-07-10 17:49:49 +0200

    Text fix

--- utils/dictionary.ts
+++ utils/dictionary.ts
@@ -2,20 +2,20 @@ export default {
   showMoreButton: "Další pohádky",
   moreFairyTales: {
     title: "Ještě nespí? Čtěte další pohádku",
-    allFairyTalesButton: "Všechny pohádky"
+    allFairyTalesButton: "Všechny pohádky",
   },
   footer: {
     copyright: {
       title: "Všechna práva vyhrazena",
       description:
-        "Rádi bychom vás upozornili, že všechny pohádky na této stránce jsou chráněny autorskými právy a jakékoliv kopírování, šíření či další použití bez našeho výslovného souhlasu je zakázáno. Naše pohádky jsou určeny pro zábavu a vzdělávání dětí a nemají za cíl urážet či poškozovat. Upozorňujeme, že se zříkáme jakékoliv odpovědnosti za případné následky čtení našich pohádek. Děkujeme za vaše pochopení.",
+        "Rádi bychom vás upozornili, že všechny pohádky na této stránce jsou chráněny autorskými právy a jakékoliv kopírování, šíření či další použití bez našeho výslovného souhlasu je zakázáno. Naše pohádky jsou určeny pro zábavu a vzdělávání dětí a nemají za cíl urážet či poškozovat. Upozorňujeme, že se zříkáme jakékoliv odpovědnosti za případné následky čtením našich pohádek. Děkujeme za vaše pochopení.",
     },
   },
   pages: {
     homepage: {
       title: "Katalog pohádek",
       description:
-        "Vítejte na webové stránce plné pohádek pro děti! Přinášíme Vám kouzelné příběhy, které budou vaše děti milovat. Naše kolekce zahrnuje moderní pohádky pro novou generaci, s neobyčejnými hrdiny a kouzelnými světy, které okouzlí i ty nejnáročnější čtenáře. Připojte se k nám a vychutnejte si kouzlo pohádek!",
+        "Čtěte pohádky pro děti online. Přinášíme Vám kouzelné příběhy, které budou vaše děti milovat. Naše kolekce zahrnuje moderní pohádky pro novou generaci, s neobyčejnými hrdiny a kouzelnými světy, které okouzlí i ty nejnáročnější čtenáře. Připojte se k nám a vychutnejte si kouzlo pohádek!",
       randomFairyTaleButton: "Číst náhodnou pohádku",
     },
     search: {
